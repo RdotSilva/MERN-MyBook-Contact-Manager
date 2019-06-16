dd261042fbdb89933c7ea6e04344eff5f0ac078d
Ryan Silva <RyandotSilva@gmail.com>
2019-06-16 11:21:36 -0400

    Add get logged in user route

---
+++ routes/auth.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+// @route   GET api/auth
+// @desc    Get logged in user
+// @access  Private
+router.get("/", (req, res) => {
+	res.send("Get logged in user");
+});
+
+module.exports = router;
