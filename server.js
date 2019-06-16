5d170b9e7f1b479ad4449046ddee686e6bfba4f1
Ryan Silva <RyandotSilva@gmail.com>
2019-06-16 11:11:45 -0400

    Add route files

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (req, res) =>
+	res.json({ msg: "Welcome to the ContactKeeper API..." })
+);
+
 const PORT = process.env.port || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
