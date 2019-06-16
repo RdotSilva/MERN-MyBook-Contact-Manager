c79f50fe5cd90df1f29210f4f38e86e50989dcb7
Ryan Silva <RyandotSilva@gmail.com>
2019-06-16 11:22:38 -0400

    Add auth user and get token route

--- routes/auth.js
+++ routes/auth.js
@@ -8,4 +8,11 @@ router.get("/", (req, res) => {
 	res.send("Get logged in user");
 });
 
+// @route   POST api/auth
+// @desc    Auth user & get token
+// @access  Public
+router.post("/", (req, res) => {
+	res.send("Log in user");
+});
+
 module.exports = router;
