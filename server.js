4f74409e636d120a3097de7e34c3406e69fe4d5b
Ryan Silva <RyandotSilva@gmail.com>
2019-06-16 11:16:38 -0400

    Define routes in server

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require("express");
 
+const users = require("./routes/users");
+const auth = require("./routes/auth");
+const contacts = require("./routes/contacts");
+
 const app = express();
 
 app.get("/", (req, res) =>
 	res.json({ msg: "Welcome to the ContactKeeper API..." })
 );
+
+// Define Routes
+app.use("/api/users", users);
+app.use("/api/auth", auth);
+app.use("/api/contacts", contacts);
 
 const PORT = process.env.port || 5000;
 
