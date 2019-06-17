1bb1ed9a136f08c30d3e3b88091a1b9269253ed3
Ryan Silva <RyandotSilva@gmail.com>
2019-06-17 15:30:46 -0400

    Add connectDB to server

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const users = require("./routes/users");
 const auth = require("./routes/auth");
 const contacts = require("./routes/contacts");
 
 const app = express();
+
+// Connect Database
+connectDB();
 
 app.get("/", (req, res) =>
 	res.json({ msg: "Welcome to the ContactKeeper API..." })
