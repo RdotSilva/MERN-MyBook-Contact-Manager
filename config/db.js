9dad3849ad9faa799ee3a3c63248e1c76c517475
Ryan Silva <RyandotSilva@gmail.com>
2019-06-17 15:28:27 -0400

    Add connectDB function

---
+++ config/db.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
+
+const connectDB = () => {
+	mongoose.connect(db, {
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useFindAndModify: false
+	});
+};
