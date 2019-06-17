c19c87b9700bc7cc6fbca3dda1064d4f9e74e346
Ryan Silva <RyandotSilva@gmail.com>
2019-06-17 15:29:53 -0400

    Update connectDB to use promise

--- config/db.js
+++ config/db.js
@@ -3,9 +3,17 @@ const config = require("config");
 const db = config.get("mongoURI");
 
 const connectDB = () => {
-	mongoose.connect(db, {
-		useNewUrlParser: true,
-		useCreateIndex: true,
-		useFindAndModify: false
-	});
+	mongoose
+		.connect(db, {
+			useNewUrlParser: true,
+			useCreateIndex: true,
+			useFindAndModify: false
+		})
+		.then(() => console.log("MongoDB Connected"))
+		.catch(err => {
+			console.error(err.message);
+			process.exit(1);
+		});
 };
+
+module.exports = connectDB;
