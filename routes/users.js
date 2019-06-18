a5d15f4de044ebf80f067def3a0b9b1306d95a75
Ryan Silva <RyandotSilva@gmail.com>
2019-06-18 15:43:11 -0400

    Add try catch

--- routes/users.js
+++ routes/users.js
@@ -19,12 +19,20 @@ router.post(
 			"Please enter a password with 6 or more characters"
 		).isLength({ min: 6 })
 	],
-	(req, res) => {
+	async (req, res) => {
 		const errors = validationResult(req);
 		if (!errors.isEmpty()) {
 			return res.status(400).json({ errors: errors.array() });
 		}
-		res.send(req.body);
+		const { name, email, password } = req.body;
+
+		try {
+			let user = await User.findOne({ email });
+
+			if (user) {
+				return res.status(400).json({ msg: "User already exists" });
+			}
+		} catch (err) {}
 	}
 );
 
