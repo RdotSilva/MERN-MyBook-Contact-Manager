decddd0610982a60db357b5073e9c2cc65876d00
Ryan Silva <RyandotSilva@gmail.com>
2019-06-18 15:35:53 -0400

    Add name validator

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,22 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator/check");
 
 const User = require("../models/User");
 
 // @route   POST api/users
 // @desc    Register a user
 // @access  public
-router.post("/", (req, res) => {
-	res.send(req.body);
-});
+router.post(
+	"/",
+	[
+		check("name", "Nme is required")
+			.not()
+			.isEmpty()
+	],
+	(req, res) => {
+		res.send(req.body);
+	}
+);
 
 module.exports = router;
