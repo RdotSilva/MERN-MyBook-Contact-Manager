78b9f6f342a2358a4997f101f2ce78ba5ca52c50
Ryan Silva <RyandotSilva@gmail.com>
2019-06-19 15:35:43 -0400

    Add logic to check for token

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,4 +4,17 @@ const config = require("config");
 module.exports = function(req, res, next) {
 	// Get token from header
 	const token = req.header("x-auth-token");
+
+	// Check if not token
+	if (!token) {
+		return res.status(401).json({ msg: "No token, authorization denied" });
+	}
+
+	try {
+		const decoded = jwt.verify(token, config.get("jwtSecret"));
+
+		req.user = decoded.user;
+	} catch (error) {
+		res.status(401).json({ msg: "Token is not valid" });
+	}
 };
