e05110fb7cb6b00e1f33d049cdde90de53c5394d
Ryan Silva <RyandotSilva@gmail.com>
2019-06-23 10:31:05 -0400

    Add about Route

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ const App = () => {
 				<div className="container">
 					<Switch>
 						<Route exact path="/" component={Home} />
+						<Route exact path="/about" component={About} />
 					</Switch>
 				</div>
 			</Fragment>
