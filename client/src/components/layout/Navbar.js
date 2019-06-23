70c10527851e03999cc06180fe631351efef49ce
Ryan Silva <RyandotSilva@gmail.com>
2019-06-23 10:26:34 -0400

    Add style and icon to Navbar

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -2,7 +2,13 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const Navbar = ({ title, icon }) => {
-	return <div />;
+	return (
+		<div className="navbar bg-primary">
+			<h1>
+				<i className={icon} /> {title}
+			</h1>
+		</div>
+	);
 };
 
 Navbar.propTypes = {
