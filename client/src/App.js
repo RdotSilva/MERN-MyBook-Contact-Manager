68b69ab4297b0abf4e7dd3f965b7ad494d6acfa0
Ryan Silva <RyandotSilva@gmail.com>
2019-06-23 10:27:32 -0400

    Add Navbar to main app

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
+import Navbar from "./components/layout/Navbar";
 import "./App.css";
 
 const App = () => {
-	return <div className="App">My App</div>;
+	return (
+		<div className="App">
+			<Navbar />
+			My App
+		</div>
+	);
 };
 
 export default App;
