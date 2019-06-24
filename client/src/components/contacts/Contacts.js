e997320cc896a8a1b262bc813b2f14bfd059bcd0
Ryan Silva <RyandotSilva@gmail.com>
2019-06-24 07:15:17 -0400

    Add imports

---
+++ client/src/components/contacts/Contacts.js
@@ -0,0 +1,8 @@
+import React, { Fragment, useContext } from "react";
+import ContactContext from "../../context/contact/contactContext";
+
+const Contacts = () => {
+	return <div />;
+};
+
+export default Contacts;
