772f35c5d965da5e19ec7c316143f25e475c5c5b
Ryan Silva <RyandotSilva@gmail.com>
2019-06-24 07:08:27 -0400

    Add hard coded contact list

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -11,3 +11,31 @@ import {
 	FILTER_CONTACTS,
 	CLEAR_FILTER
 } from "../types";
+
+const ContactState = props => {
+	const initialState = {
+		contacts: [
+			{
+				id: 1,
+				name: "Fake Name",
+				email: "fake@fake.com",
+				phone: "111-111-1111",
+				type: "personal"
+			},
+			{
+				id: 2,
+				name: "Fake Guy",
+				email: "fakeguy@fake.com",
+				phone: "222-111-1111",
+				type: "personal"
+			},
+			{
+				id: 1,
+				name: "Fake Girlme",
+				email: "fakegirl@fake.com",
+				phone: "333-111-1111",
+				type: "professional"
+			}
+		]
+	};
+};
