13104bc733f2b19e10b4d3d5521520cc11a686c9
Ryan Silva <RyandotSilva@gmail.com>
2019-06-24 07:16:43 -0400

    Update Contacts component

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -2,7 +2,16 @@ import React, { Fragment, useContext } from "react";
 import ContactContext from "../../context/contact/contactContext";
 
 const Contacts = () => {
-	return <div />;
+	const contactContext = useContext(ContactContext);
+
+	const { contacts } = contactContext;
+	return (
+		<Fragment>
+			{contacts.map(contact => (
+				<h3>{contact.name}</h3>
+			))}
+		</Fragment>
+	);
 };
 
 export default Contacts;
