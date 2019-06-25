013b583c893d0ee72f399db62f586f9f951816c5
Ryan Silva <RyandotSilva@gmail.com>
2019-06-25 09:37:41 -0400

    Add useState

--- client/src/components/contacts/ContactForm.js
+++ client/src/components/contacts/ContactForm.js
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ContactForm = () => {
+	const [contact, setContact] = useState({
+		name: "",
+		email: "",
+		phone: "",
+		type: "personal"
+	});
+
 	return <div />;
 };
 
