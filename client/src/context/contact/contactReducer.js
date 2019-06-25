edee1ff9fe08e6266afae86f9c9c71c5cd6393a9
Ryan Silva <RyandotSilva@gmail.com>
2019-06-25 09:58:21 -0400

    Add add contact switch case

--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -7,3 +7,15 @@ import {
 	FILTER_CONTACTS,
 	CLEAR_FILTER
 } from "../types";
+
+export default (state, action) => {
+	switch (action.type) {
+		case ADD_CONTACT:
+			return {
+				...state,
+				contacts: [...state.contacts, action.payload]
+			};
+		default:
+			return state;
+	}
+};
