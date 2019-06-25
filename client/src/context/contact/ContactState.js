566ef229dab1d9beea06229c4b9e68687a90f88b
Ryan Silva <RyandotSilva@gmail.com>
2019-06-25 09:55:53 -0400

    Add addContact method

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -42,6 +42,10 @@ const ContactState = props => {
 	const [state, dispatch] = useReducer(contactReducer, initialState);
 
 	// Add Contact
+	const addContact = contact => {
+		contact.id = uuid.v4();
+		dispatch({ type: ADD_CONTACT, payload: contact });
+	};
 
 	// Delete Contact
 
