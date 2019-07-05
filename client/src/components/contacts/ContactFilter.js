1000129b034c7c8539365eebdc82ec46acb14c1b
Ryan Silva <RyandotSilva@gmail.com>
2019-07-05 15:52:50 -0400

    Add filter input

--- client/src/components/contacts/ContactFilter.js
+++ client/src/components/contacts/ContactFilter.js
@@ -1,9 +1,18 @@
-import React, { useContext } from "react";
+import React, { useContext, useRef } from "react";
 import ContactContext from "../../context/contact/contactContext";
 
 const ContactFilter = () => {
 	const contactContext = useContext(ContactContext);
-	return <div />;
+	return (
+		<form>
+			<input
+				ref={text}
+				type="text"
+				placeholder="Filter Contacts..."
+				onChange={onChange}
+			/>
+		</form>
+	);
 };
 
 export default ContactFilter;
