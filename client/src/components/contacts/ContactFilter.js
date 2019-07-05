45c02c0527ce42737d3edb177dbe44ac381241f8
Ryan Silva <RyandotSilva@gmail.com>
2019-07-05 15:56:08 -0400

    Add useEffect function

--- client/src/components/contacts/ContactFilter.js
+++ client/src/components/contacts/ContactFilter.js
@@ -1,15 +1,23 @@
-import React, { useContext, useRef } from "react";
+import React, { useContext, useRef, useEffect } from "react";
 import ContactContext from "../../context/contact/contactContext";
 
 const ContactFilter = () => {
 	const contactContext = useContext(ContactContext);
 	const text = useRef("");
 
+	const { filterContacts, clearFilter, filtered } = contactContext;
+
+	useEffect(() => {
+		if (filtered === null) {
+			text.current.value = "";
+		}
+	});
+
 	const onChange = e => {
 		if (text.current.value !== "") {
-			contactContext.filterContacts(e.target.value);
+			filterContacts(e.target.value);
 		} else {
-			contactContext.clearFilter();
+			clearFilter();
 		}
 	};
 
