bc255dc56d1cd17d08c35382fd213c4ca85a3d15
Ryan Silva <RyandotSilva@gmail.com>
2019-07-05 15:48:40 -0400

    Add filter contacts case

--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -39,6 +39,14 @@ export default (state, action) => {
 				...state,
 				current: null
 			};
+		case FILTER_CONTACTS:
+			return {
+				...state,
+				filtered: state.contacts.filter(contact => {
+					const regex = new RegExp(`${action.payload}`, "gi");
+					return contact.name.match(regex) || contact.email.match(regex);
+				})
+			};
 		default:
 			return state;
 	}
