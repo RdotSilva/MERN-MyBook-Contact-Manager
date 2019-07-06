b905d5f9012858cc33155108b3f4214092ca10db
Ryan Silva <RyandotSilva@gmail.com>
2019-07-06 14:34:05 -0400

    Add Login form

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -6,6 +6,35 @@ const Login = () => {
 		password: ""
 	});
 
-	
+	const { email, password } = user;
+
+	return (
+		<div className="form-container">
+			<h1>
+				Account <span className="text-primary">Login</span>
+			</h1>
+			<form onSubmit={onSubmit}>
+				<div className="form-group">
+					<label htmlFor="email">Email</label>
+					<input type="email" name="email" value={email} onChange={onChange} />
+				</div>
+				<div className="form-group">
+					<label htmlFor="password">Password</label>
+					<input
+						type="password"
+						name="password"
+						value={password}
+						onChange={onChange}
+					/>
+				</div>
+				<input
+					type="submit"
+					value="Login"
+					className="btn btn-primary btn-block"
+				/>
+			</form>
+		</div>
+	);
+};
 
 export default Login;
