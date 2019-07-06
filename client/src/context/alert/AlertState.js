acd1ed91956603749774e4b753e10178904ce72b
Ryan Silva <RyandotSilva@gmail.com>
2019-07-06 17:06:04 -0400

    Add setAlert action

--- client/src/context/alert/AlertState.js
+++ client/src/context/alert/AlertState.js
@@ -1,4 +1,5 @@
 import React, { useReducer } from "react";
+import uuid from "uuid";
 import AlertContext from "./alertContext";
 import alertReducer from "./alertReducer";
 import { SET_ALERT, REMOVE_ALERT } from "../types";
@@ -7,6 +8,17 @@ const AlertState = props => {
 	const initialState = [];
 
 	const [state, dispatch] = useReducer(alertReducer, initialState);
+
+	// Set Alert
+	const setAlert = (msg, type, timeout = 5000) => {
+		const id = uuid.v4();
+		dispatch({
+			type: SET_ALERT,
+			payload: { msg, type, id }
+		});
+
+		setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), timeout);
+	};
 };
 
 export default AlertState;
