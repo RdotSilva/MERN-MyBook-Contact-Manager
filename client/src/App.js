2699dba175a69ce9957727f0cfb8b70526523b44
Ryan Silva <RyandotSilva@gmail.com>
2019-07-06 14:34:59 -0400

    Add Login route to main app

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
+import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import ContactState from "./context/contact/ContactState";
 import AuthState from "./context/auth/AuthState";
@@ -20,6 +21,7 @@ const App = () => {
 								<Route exact path="/" component={Home} />
 								<Route exact path="/about" component={About} />
 								<Route exact path="/register" component={Register} />
+								<Route exact path="/login" component={Login} />
 							</Switch>
 						</div>
 					</Fragment>
