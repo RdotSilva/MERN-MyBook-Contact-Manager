7ca30ccd01273013c710cc7f23811fabb2c1c03b
Ryan Silva <RyandotSilva@gmail.com>
2019-07-06 17:13:17 -0400

    Add logic to show alerts if any

--- client/src/components/layout/Alerts.js
+++ client/src/components/layout/Alerts.js
@@ -3,7 +3,14 @@ import AlertContext from "../../context/alert/alertContext";
 
 const Alerts = () => {
 	const alertContext = useContext(AlertContext);
-	return <div />;
+	return (
+		alertContext.alerts.length > 0 &&
+		alertContext.alerts.map(alert => (
+			<div key={alert.id} className={`alert alert-${alert.type}`}>
+				<i className="fas fa-info circle" /> {alert.msg}
+			</div>
+		))
+	);
 };
 
 export default Alerts;
