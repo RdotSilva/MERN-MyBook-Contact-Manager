01e3e9ec37dfde5c41020575257f82035d6d5e60
Ryan Silva <RyandotSilva@gmail.com>
2019-07-06 17:14:48 -0400

    Add Alerts to main app

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./components/layout/Navbar";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 import Login from "./components/auth/Login";
+import Alerts from "./components/layout/Alerts";
 import Register from "./components/auth/Register";
 import ContactState from "./context/contact/ContactState";
 import AuthState from "./context/auth/AuthState";
@@ -19,12 +20,14 @@ const App = () => {
 						<Fragment>
 							<Navbar />
 							<div className="container">
-								<Switch>
-									<Route exact path="/" component={Home} />
-									<Route exact path="/about" component={About} />
-									<Route exact path="/register" component={Register} />
-									<Route exact path="/login" component={Login} />
-								</Switch>
+								<Alerts>
+									<Switch>
+										<Route exact path="/" component={Home} />
+										<Route exact path="/about" component={About} />
+										<Route exact path="/register" component={Register} />
+										<Route exact path="/login" component={Login} />
+									</Switch>
+								</Alerts>
 							</div>
 						</Fragment>
 					</Router>
