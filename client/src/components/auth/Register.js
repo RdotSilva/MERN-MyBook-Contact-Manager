961c63fe7a371232a586ebf5b59de9783d4480de
Ryan Silva <RyandotSilva@gmail.com>
2019-07-06 14:25:02 -0400

    Add onSubmit function

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -8,33 +8,52 @@ const Register = () => {
 		password2: ""
 	});
 
-  const { name, email, password, password2 } = user;
-  
-  const onChange = e => setUser({...user, [e.target.name]: e.target.value})
+	const { name, email, password, password2 } = user;
+
+	const onChange = e => setUser({ ...user, [e.target.name]: e.target.value });
+
+	const onSubmit = e => {
+		e.preventDefault();
+		console.log("Register Submit");
+	};
 
 	return (
 		<div className="form-container">
 			<h1>
 				Account <span className="text-primary">Register</span>
 			</h1>
-			<form>
+			<form onSubmit={onSubmit}>
 				<div className="form-group">
 					<label htmlFor="name">Name</label>
-          <input type='text' name='name' value={name} onChange={} />
+					<input type="text" name="name" value={name} onChange={onChange} />
 				</div>
-        <div className="form-group">
+				<div className="form-group">
 					<label htmlFor="email">Email</label>
-          <input type='email' name='email' value={email} onChange={} />
+					<input type="email" name="email" value={email} onChange={onChange} />
 				</div>
-        <div className="form-group">
+				<div className="form-group">
 					<label htmlFor="password">Password</label>
-          <input type='password' name='password' value={password} onChange={} />
+					<input
+						type="password"
+						name="password"
+						value={password}
+						onChange={onChange}
+					/>
 				</div>
-        <div className="form-group">
+				<div className="form-group">
 					<label htmlFor="password2">Confirm Password</label>
-          <input type='password2' name='password2' value={password2} onChange={} />
+					<input
+						type="password2"
+						name="password2"
+						value={password2}
+						onChange={onChange}
+					/>
 				</div>
-        <input type='submit' value='Register' className='btn btn-primary btn-block' />
+				<input
+					type="submit"
+					value="Register"
+					className="btn btn-primary btn-block"
+				/>
 			</form>
 		</div>
 	);
