73497e0a6b1f4d37c0688e5a2f6bb7b24127accb
Ryan Silva <RyandotSilva@gmail.com>
2019-07-08 20:12:48 -0400

    Add setAuthToken to loadUser function

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -2,6 +2,7 @@ import React, { useReducer } from "react";
 import AuthContext from "./authContext";
 import authReducer from "./authReducer";
 import axios from "axios";
+import setAuthToken from "../../utils/setAuthToken";
 import {
 	REGISTER_SUCCESS,
 	REGISTER_FAIL,
@@ -26,7 +27,9 @@ const AuthState = props => {
 
 	// Load User
 	const loadUser = async () => {
-		// @todo - load token into global headers
+		if (localStorage.token) {
+			setAuthToken(localStorage.token);
+		}
 
 		try {
 			const res = await axios.get("/api/auth");
