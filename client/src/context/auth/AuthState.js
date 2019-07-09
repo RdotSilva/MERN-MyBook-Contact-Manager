b1db89a22592da9f1c442a538e9c93767f8637f9
Ryan Silva <RyandotSilva@gmail.com>
2019-07-09 17:12:10 -0400

    Update login function with dispatch

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -70,7 +70,30 @@ const AuthState = props => {
 	};
 
 	// Login User
-	const login = () => console.log("login");
+	const login = async formData => {
+		const config = {
+			headers: {
+				"Content-Type": "application/json"
+			}
+		};
+
+		try {
+			const res = await axios.post("/api/auth", formData, config);
+
+			dispatch({
+				type: LOGIN_SUCCESS,
+				// res.data is going to be the token sent from the api route
+				payload: res.data
+			});
+
+			loadUser();
+		} catch (err) {
+			dispatch({
+				type: LOGIN_FAIL,
+				payload: err.response.data.msg
+			});
+		}
+	};
 
 	// Logout
 	const logout = () => console.log("logout");
