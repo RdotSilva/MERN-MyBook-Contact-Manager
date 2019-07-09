f761a748b8ad3bd4a1c0d544ebbba5ff9f0fb379
Ryan Silva <RyandotSilva@gmail.com>
2019-07-08 20:18:15 -0400

    Add authentication redirect inside useEffect

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -2,20 +2,24 @@ import React, { useState, useContext, useEffect } from "react";
 import AlertContext from "../../context/alert/alertContext";
 import AuthContext from "../../context/auth/authContext";
 
-const Register = () => {
+const Register = props => {
 	const alertContext = useContext(AlertContext);
 	const authContext = useContext(AuthContext);
 
 	const { setAlert } = alertContext;
 
-	const { register, error, clearErrors } = authContext;
+	const { register, error, clearErrors, isAuthenticated } = authContext;
 
 	useEffect(() => {
+		if (isAuthenticated) {
+			// Redirect if user is authenticated.
+			props.history.push("/");
+		}
 		if (error === "User already exists") {
 			setAlert(error, "danger");
 			clearErrors();
 		}
-	}, [error]);
+	}, [error, isAuthenticated, props.history]);
 
 	const [user, setUser] = useState({
 		name: "",
