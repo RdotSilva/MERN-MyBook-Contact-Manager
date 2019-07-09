f543be55a6e5d0f65aa648850fe01d5b1b6b00dc
Ryan Silva <RyandotSilva@gmail.com>
2019-07-08 20:14:04 -0400

    Add setAuthToken to main app

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,12 @@ import Register from "./components/auth/Register";
 import ContactState from "./context/contact/ContactState";
 import AuthState from "./context/auth/AuthState";
 import AlertState from "./context/alert/AlertState";
+import setAuthToken from "./utils/setAuthToken";
 import "./App.css";
+
+if (localStorage.token) {
+	setAuthToken(localStorage.token);
+}
 
 const App = () => {
 	return (
