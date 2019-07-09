20a6bb3df2904c3fb1cb7283bb4d237b2006d948
Ryan Silva <RyandotSilva@gmail.com>
2019-07-09 17:37:52 -0400

    Add guestLinks

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -20,25 +20,23 @@ const Navbar = ({ title, icon }) => {
 		</Fragment>
 	);
 
+	const guestLinks = (
+		<Fragment>
+			<li>
+				<Link to="/register">Register</Link>
+			</li>
+			<li>
+				<Link to="/login">Login</Link>
+			</li>
+		</Fragment>
+	);
+
 	return (
 		<div className="navbar bg-primary">
 			<h1>
 				<i className={icon} /> {title}
 			</h1>
-			<ul>
-				<li>
-					<Link to="/">Home</Link>
-				</li>
-				<li>
-					<Link to="/about">About</Link>
-				</li>
-				<li>
-					<Link to="/register">Register</Link>
-				</li>
-				<li>
-					<Link to="/login">Login</Link>
-				</li>
-			</ul>
+			<ul>{}</ul>
 		</div>
 	);
 };
