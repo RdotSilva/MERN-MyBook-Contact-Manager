b0c8eddc5e7b83d0d9bea50aff236596cb6cf1c9
Ryan Silva <RyandotSilva@gmail.com>
2019-07-09 17:19:00 -0400

    Update onSubmit function to check for valid fields

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -2,7 +2,7 @@ import React, { useState, useContext, useEffect } from "react";
 import AuthContext from "../../context/auth/authContext";
 import AlertContext from "../../context/alert/alertContext";
 
-const Login = () => {
+const Login = props => {
 	const alertContext = useContext(AlertContext);
 	const authContext = useContext(AuthContext);
 
@@ -33,7 +33,14 @@ const Login = () => {
 
 	const onSubmit = e => {
 		e.preventDefault();
-		console.log("Login Submit");
+		if (email === "" || password === "") {
+			setAlert("Please fill in all fields", "danger");
+		} else {
+			login({
+				email,
+				password
+			});
+		}
 	};
 
 	return (
