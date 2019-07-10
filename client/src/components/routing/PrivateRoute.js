680b660a0ca1cfb1193c471bc43b278c8c726959
Ryan Silva <RyandotSilva@gmail.com>
2019-07-10 18:46:15 -0400

    Add authContext to PrivateRoute

--- client/src/components/routing/PrivateRoute.js
+++ client/src/components/routing/PrivateRoute.js
@@ -1,6 +1,10 @@
 import React, { useContext } from "react";
+import AuthContext from "../../context/auth/authContext";
 
-const PrivateRoute = () => {
+const PrivateRoute = ({ component: Component, ...rest }) => {
+	const authContext = useContext(AuthContext);
+	const { isAuthenticated, loading } = authContext;
+
 	return <div />;
 };
 
