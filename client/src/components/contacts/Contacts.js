3f01636e13c86a997b088312a80e2e1c0ac9da64
Ryan Silva <RyandotSilva@gmail.com>
2019-07-12 14:44:46 -0400

    Add spinner loading logic

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -2,6 +2,7 @@ import React, { Fragment, useContext, useEffect } from "react";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 import ContactItem from "./ContactItem";
 import ContactContext from "../../context/contact/contactContext";
+import Spinner from "../layout/Spinner";
 
 const Contacts = () => {
 	const contactContext = useContext(ContactContext);
@@ -19,19 +20,31 @@ const Contacts = () => {
 
 	return (
 		<Fragment>
-			<TransitionGroup>
-				{filtered !== null
-					? filtered.map(contact => (
-							<CSSTransition key={contact._id} timeout={500} classNames="item">
-								<ContactItem contact={contact} />
-							</CSSTransition>
-					  ))
-					: contacts.map(contact => (
-							<CSSTransition key={contact._id} timeout={500} classNames="item">
-								<ContactItem contact={contact} />
-							</CSSTransition>
-					  ))}
-			</TransitionGroup>
+			{contacts !== null && !loading ? (
+				<TransitionGroup>
+					{filtered !== null
+						? filtered.map(contact => (
+								<CSSTransition
+									key={contact._id}
+									timeout={500}
+									classNames="item"
+								>
+									<ContactItem contact={contact} />
+								</CSSTransition>
+						  ))
+						: contacts.map(contact => (
+								<CSSTransition
+									key={contact._id}
+									timeout={500}
+									classNames="item"
+								>
+									<ContactItem contact={contact} />
+								</CSSTransition>
+						  ))}
+				</TransitionGroup>
+			) : (
+				<Spinner />
+			)}
 		</Fragment>
 	);
 };
