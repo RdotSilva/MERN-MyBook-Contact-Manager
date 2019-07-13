0c0d79f46714053ad11ec1750922d3c6b270e4c4
Ryan Silva <RyandotSilva@gmail.com>
2019-07-13 19:35:11 -0400

    Add production build check inside server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 
 const users = require("./routes/users");
 const auth = require("./routes/auth");
@@ -21,6 +22,17 @@ app.get("/", (req, res) =>
 app.use("/api/users", users);
 app.use("/api/auth", auth);
 app.use("/api/contacts", contacts);
+
+// Serve static assets in production (react)
+if (process.env.NODE_ENV === "production") {
+	// Set static folder
+	app.use(express.static("client/build"));
+
+	// Get ANYTHING that is not the api routes above
+	app.get("*", (req, res) =>
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+	);
+}
 
 const PORT = process.env.port || 5000;
 
