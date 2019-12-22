75c2999f26376d00cd0e7116ce6c02876e01db72
RdotSilva <RyandotSilva@gmail.com>
2019-12-21 21:20:04 -0500

    Update connectDB to use env mongo uri

--- config/db.js
+++ config/db.js
@@ -1,19 +1,17 @@
 const mongoose = require("mongoose");
-const config = require("config");
-const db = config.get("mongoURI");
 
 const connectDB = async () => {
-	try {
-		await mongoose.connect(db, {
-			useNewUrlParser: true,
-			useCreateIndex: true,
-			useFindAndModify: false
-		});
-		console.log("MongoDB Connected...");
-	} catch (err) {
-		console.error(err.message);
-		process.exit(1);
-	}
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    });
+    console.log("MongoDB Connected...");
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
