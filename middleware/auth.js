5e966c635dd90a9133b08f097453051e34b6b1ef
RdotSilva <RyandotSilva@gmail.com>
2019-12-21 21:23:28 -0500

    Update auth middleware to use env variable

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,21 +1,20 @@
 const jwt = require("jsonwebtoken");
-const config = require("config");
 
 module.exports = function(req, res, next) {
-	// Get token from header
-	const token = req.header("x-auth-token");
+  // Get token from header
+  const token = req.header("x-auth-token");
 
-	// Check if not token
-	if (!token) {
-		return res.status(401).json({ msg: "No token, authorization denied" });
-	}
+  // If no token present send error
+  if (!token) {
+    return res.status(401).json({ msg: "No token, authorization denied" });
+  }
 
-	try {
-		const decoded = jwt.verify(token, config.get("jwtSecret"));
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-		req.user = decoded.user;
-		next();
-	} catch (error) {
-		res.status(401).json({ msg: "Token is not valid" });
-	}
+    req.user = decoded.user;
+    next();
+  } catch (error) {
+    res.status(401).json({ msg: "Token is not valid" });
+  }
 };
