35b31565d30769e61f2e0be117af897ad446cc17
RdotSilva <RyandotSilva@gmail.com>
2019-12-21 21:25:03 -0500

    Update register route to use env variable

--- routes/users.js
+++ routes/users.js
@@ -11,65 +11,65 @@ const User = require("../models/User");
 // @desc    Register a user
 // @access  public
 router.post(
-	"/",
-	[
-		check("name", "Please add name")
-			.not()
-			.isEmpty(),
-		check("email", "Please include a valid email").isEmail(),
-		check(
-			"password",
-			"Please enter a password with 6 or more characters"
-		).isLength({ min: 6 })
-	],
-	async (req, res) => {
-		const errors = validationResult(req);
-		if (!errors.isEmpty()) {
-			return res.status(400).json({ errors: errors.array() });
-		}
-		const { name, email, password } = req.body;
+  "/",
+  [
+    check("name", "Please add name")
+      .not()
+      .isEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({ min: 6 })
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { name, email, password } = req.body;
 
-		try {
-			let user = await User.findOne({ email });
+    try {
+      let user = await User.findOne({ email });
 
-			if (user) {
-				return res.status(400).json({ msg: "User already exists" });
-			}
+      if (user) {
+        return res.status(400).json({ msg: "User already exists" });
+      }
 
-			user = new User({
-				name,
-				email,
-				password
-			});
+      user = new User({
+        name,
+        email,
+        password
+      });
 
-			const salt = await bcrypt.genSalt(10);
+      const salt = await bcrypt.genSalt(10);
 
-			user.password = await bcrypt.hash(password, salt);
+      user.password = await bcrypt.hash(password, salt);
 
-			await user.save();
+      await user.save();
 
-			const payload = {
-				user: {
-					id: user.id
-				}
-			};
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
 
-			jwt.sign(
-				payload,
-				config.get("jwtSecret"),
-				{
-					expiresIn: 360000
-				},
-				(err, token) => {
-					if (err) throw err;
-					res.json({ token });
-				}
-			);
-		} catch (err) {
-			console.error(err.message);
-			res.status(500).send("Server Error");
-		}
-	}
+      jwt.sign(
+        payload,
+        process.env.JWT_SECRET,
+        {
+          expiresIn: 360000
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
 );
 
 module.exports = router;
