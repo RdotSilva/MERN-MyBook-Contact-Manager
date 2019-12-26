1e181f3fbeef101f89361dcee5421c50a7142406
RdotSilva <RyandotSilva@gmail.com>
2019-12-26 11:13:11 -0500

    Update login page with test account

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -3,73 +3,76 @@ import AuthContext from "../../context/auth/authContext";
 import AlertContext from "../../context/alert/alertContext";
 
 const Login = props => {
-	const alertContext = useContext(AlertContext);
-	const authContext = useContext(AuthContext);
+  const alertContext = useContext(AlertContext);
+  const authContext = useContext(AuthContext);
 
-	const { setAlert } = alertContext;
+  const { setAlert } = alertContext;
 
-	const { login, error, clearErrors, isAuthenticated } = authContext;
+  const { login, error, clearErrors, isAuthenticated } = authContext;
 
-	useEffect(() => {
-		if (isAuthenticated) {
-			// Redirect if user is authenticated.
-			props.history.push("/");
-		}
-		if (error === "Invalid Credentials") {
-			setAlert(error, "danger");
-			clearErrors();
-		}
-		// eslint-disable-next-line
-	}, [error, isAuthenticated, props.history]);
+  useEffect(() => {
+    if (isAuthenticated) {
+      // Redirect if user is authenticated.
+      props.history.push("/");
+    }
+    if (error === "Invalid Credentials") {
+      setAlert(error, "danger");
+      clearErrors();
+    }
+    // eslint-disable-next-line
+  }, [error, isAuthenticated, props.history]);
 
-	const [user, setUser] = useState({
-		email: "",
-		password: ""
-	});
+  const [user, setUser] = useState({
+    email: "",
+    password: ""
+  });
 
-	const { email, password } = user;
+  const { email, password } = user;
 
-	const onChange = e => setUser({ ...user, [e.target.name]: e.target.value });
+  const onChange = e => setUser({ ...user, [e.target.name]: e.target.value });
 
-	const onSubmit = e => {
-		e.preventDefault();
-		if (email === "" || password === "") {
-			setAlert("Please fill in all fields", "danger");
-		} else {
-			login({
-				email,
-				password
-			});
-		}
-	};
+  const onSubmit = e => {
+    e.preventDefault();
+    if (email === "" || password === "") {
+      setAlert("Please fill in all fields", "danger");
+    } else {
+      login({
+        email,
+        password
+      });
+    }
+  };
 
-	return (
-		<div className="form-container">
-			<h1>
-				Account <span className="text-primary">Login</span>
-			</h1>
-			<form onSubmit={onSubmit}>
-				<div className="form-group">
-					<label htmlFor="email">Email</label>
-					<input type="email" name="email" value={email} onChange={onChange} />
-				</div>
-				<div className="form-group">
-					<label htmlFor="password">Password</label>
-					<input
-						type="password"
-						name="password"
-						value={password}
-						onChange={onChange}
-					/>
-				</div>
-				<input
-					type="submit"
-					value="Login"
-					className="btn btn-primary btn-block"
-				/>
-			</form>
-		</div>
-	);
+  return (
+    <div className="form-container">
+      <h1>
+        Account <span className="text-primary">Login</span>
+      </h1>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" value={email} onChange={onChange} />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={onChange}
+          />
+        </div>
+        <input
+          type="submit"
+          value="Login"
+          className="btn btn-primary btn-block"
+        />
+        <p className="text-center">
+          Test Email: <b>testing@test.com</b> Test Password: <b>test123</b>
+        </p>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
